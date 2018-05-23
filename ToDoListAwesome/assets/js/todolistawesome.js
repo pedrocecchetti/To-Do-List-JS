f9eaaffbbfa39c463cc1c35f82a01a2b52c8f562
Pedro Vaz <pedro.cecchetti@gmail.com>
2018-05-23 00:10:59 -0300

    Adicionando os arquivos já existentes e testando o github, ainda meio dificil de entender

---
+++ ToDoListAwesome/assets/js/todolistawesome.js
@@ -0,0 +1,27 @@
+$("ul").on("click","li", doneclass);
+
+$('ul').on("click","li span",deletaritem);
+
+$("input[type='text'").on('keypress',enterPress);
+
+
+function deletaritem(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();	
+	})
+	event.stopPropagation();
+}
+
+function enterPress(event){
+	if(event.which === 13 ){
+		var toDoText = $(this).val();
+		var htmlToDoText = "<li><span>X </span>" + toDoText + "</li>" 
+		$('ul').append(htmlToDoText);
+		$(this).val("");
+	}
+}
+
+
+function doneclass(){
+	$(this).toggleClass('done');
+}
